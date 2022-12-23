4700b28f4546a179de52f7d70531cca56cddf0cc
Radhika Bitla <bitlaradhika3@gmail.com>
2022-12-23 22:01:45 +0530

    added login and sign up buttons in header tab

--- src/main/java/com/example/supplychainproject/SupplyChainMain.java
+++ src/main/java/com/example/supplychainproject/SupplyChainMain.java
@@ -11,6 +11,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -24,13 +25,33 @@ Pane bodyPane = new Pane();
         TextField searchArea=new TextField();
         Button searchButton=new Button("Search");
 
+        Button LoginButton=new Button("Login In");
+
+        Button signInButton=new Button("Sign Up");
         GridPane gridPane=new GridPane();
-        gridPane.setHgap(10);
+        gridPane.setHgap(20);
         gridPane.setMinSize(bodyPane.getMinWidth(),headerbar);
-        gridPane.setAlignment(Pos.CENTER_RIGHT);
+        gridPane.setAlignment(Pos.CENTER);
         gridPane.setStyle("-fx-background-color:black;");
-        gridPane.add(searchArea,0,0);
-        gridPane.add(searchButton,1,0);
+        gridPane.add(LoginButton,0,0);
+        gridPane.add(signInButton,1,0);
+        gridPane.add(searchArea,2,0);
+        gridPane.add(searchButton,3,0);
+
+
+        LoginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                bodyPane.getChildren().add(loginPage());
+            }
+        });
+
+        signInButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                bodyPane.getChildren().add(signPage());
+            }
+        });
         return gridPane;
     }
     public GridPane loginPage()
@@ -76,6 +97,53 @@ Pane bodyPane = new Pane();
             }
         });
 
+        return gridPane;
+    }
+    public GridPane signPage()
+    {
+        GridPane gridPane=new GridPane();
+
+        Label email=new Label("Email");
+        Label password=new Label("Password");
+        Label confirmPassword=new Label("Confirm Password");
+
+        TextField emailField=new TextField();
+        PasswordField passwordField=new PasswordField();
+        PasswordField confirmPasswordField=new PasswordField();
+
+        Button Signup=new Button("Sign Up");
+
+        Label messageLabel=new Label();
+
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
+        gridPane.setMinSize(bodyPane.getMinWidth(),bodyPane.getMinHeight());
+        gridPane.setStyle("-fx-background-color:grey;");
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.add(email,1,1);
+        gridPane.add(password,1,2);
+        gridPane.add(confirmPassword,1,3);
+        gridPane.add(emailField,2,1);
+        gridPane.add(passwordField,2,2);
+        gridPane.add(confirmPasswordField,2,3);
+        gridPane.add(Signup,2,4);
+        gridPane.add(messageLabel,2,5);
+
+        Signup.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if(!passwordField.getText().equals(confirmPasswordField.getText()))
+                {
+                    messageLabel.setText("Password doesn't match");
+                    messageLabel.setTextFill(Color.RED);
+                }
+                else
+                {
+                    messageLabel.setText("Sign Up Successful");
+                    messageLabel.setTextFill(Color.GREEN);
+                }
+            }
+        });
         return gridPane;
     }
 ProductDetails productDetails=new ProductDetails();
